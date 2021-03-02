db83786790931f6eea5646f4990071b636066336
selja2798 <siotong2798@gmail.com>
2021-03-02 23:52:31 +0700

    remove project file

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import Button from "@material-ui/core/Button";
 
 function App() {
   return (
@@ -9,6 +10,10 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+
+        <Button variant="contained" color="default">
+          Hello
+        </Button>
         <a
           className="App-link"
           href="https://reactjs.org"
